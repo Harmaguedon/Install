f15cc09b3da2f2c39c063b599ad7d3827d827be5
armand Jouanneau <armand.jouanneau@csgroup.eu>
2020-12-06 19:16:19 +0100

    Init

---
+++ gitkraken.bash
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+wget https://release.gitkraken.com/linux/gitkraken-amd64.deb
+sudo dpkg -i gitkraken-amd64.deb
+rm gitkraken-amd64.deb
---
+++ terminal.bash
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Install
+sudo apt install -y terminator
+
+# Keybord Shortcut
+gsettings set org.gnome.desktop.default-applications.terminal exec "terminator"
+
+# Config
+mkdir -p $HOME/.config
+cat << 'EOF' >$HOME/.config/terminator/config
+[global_config]
+  window_state = fullscreen
+  borderless = True
+[keybindings]
+  split_horiz = <Primary><Shift>h
+  split_vert = <Primary><Shift>j
+  next_tab = <Primary>n
+  prev_tab = <Primary>p
+  hide_window = twosuperior
+[profiles]
+  [[default]]
+    cursor_color = "#aaaaaa"
+    show_titlebar = False
+[layouts]
+  [[default]]
+    [[[window0]]]
+      type = Window
+      parent = ""
+    [[[child1]]]
+      type = Terminal
+      parent = window0
+[plugins]
+EOF
