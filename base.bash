398c943d5688679e9e02ee2eedeb4d302e3e6eda
harmaguedon <armand.jouanneau@protonmail.com>
2020-12-16 15:36:35 +0100

    Add misc tools

--- base.bash
+++ base.bash
@@ -1,11 +1,21 @@
 #!/bin/bash
 
+# Install packets
 sudo apt update && sudo apt install -y \
     curl \
     git \
+    htop \
     jq \
     lftp \
     libavcodec-dev \
-    unzip 
+    nmap \
+    transmission \
+    unzip \
+    wget
 
+# Install Calibre
+sudo apt install xdg-utils libxcb-xinerama0
+curl -s https://download.calibre-ebook.com/linux-installer.sh | sudo sh /dev/stdin
+
+# Setup wallpaper
 gsettings set org.gnome.desktop.background picture-uri $( cd "${0%/*}" && pwd )/wallpaper.png
