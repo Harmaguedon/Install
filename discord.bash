55b9165093985475e5837ffdf5210f3f07cb930e
harmaguedon <armand.jouanneau@protonmail.com>
2021-01-22 14:04:35 +0100

    Discord+Vagrant fix dependencies

--- discord.bash
+++ discord.bash
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+sudo apt install -y libappindicator1 libc++1 libc++1-10 libc++abi1-10 libdbusmenu-gtk4
 wget -O /tmp/discord.deb 'https://discord.com/api/download?platform=linux&format=deb'
 sudo dpkg -i /tmp/discord.deb 
 rm /tmp/discord.deb 
--- vagrant.bash
+++ vagrant.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo apt update 
-sudo apt install -y virtualbox virtualbox-ext-pack
+sudo apt install -y linux-headers-generic virtualbox virtualbox-dkms virtualbox-ext-pack 
 wget -O /tmp/vagrant.deb https://releases.hashicorp.com/vagrant/2.2.13/vagrant_2.2.13_x86_64.deb
 sudo apt install /tmp/vagrant.deb
 rm /tmp/vagrant.deb
