19a4f034d4461a25fb75991684925b4da676b504
harmaguedon <armand.jouanneau@protonmail.com>
2021-02-14 16:02:05 +0100

    ansible

---
+++ ansible.bash
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Install
+# sudo apt update
+# sudo apt install -y software-properties-common
+# sudo apt-add-repository -y --update ppa:ansible/ansible
+# sudo apt install -y ansible
+
+pip3 install ansible==2.9.16
