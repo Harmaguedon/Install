d0cfa1741a19cc486d4e8a3fd11201c0c966c80d
harmaguedon <armand.jouanneau@protonmail.com>
2021-02-14 15:59:19 +0100

    firefox plugins

--- firefox.bash
+++ firefox.bash
@@ -7,7 +7,10 @@ sudo apt install -y firefox
 # Plugins
 ## Cookie Quick Manager
 ## Dark Reader
+## Decentraleys
+## NinjaCookie
 ## NoScript 
+## Open In Chrome Browser
 ## SponsorBlock
 ## uBlock Origin
 ## Youtube Audio
