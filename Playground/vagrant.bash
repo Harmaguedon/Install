d718f8ec9a4360324482c4a53fa415aaf8bc434a
Harma <40046005+Harmaguedon@users.noreply.github.com>
2022-01-17 09:43:09 +0100

    Alaways install the latest version of vagrant + EFI modules signing

--- Playground/vagrant.bash
+++ Playground/vagrant.bash
@@ -2,6 +2,9 @@
 
 sudo apt update 
 sudo apt install -y linux-headers-generic virtualbox virtualbox-dkms virtualbox-ext-pack 
-wget -O /tmp/vagrant.deb https://releases.hashicorp.com/vagrant/2.2.13/vagrant_2.2.13_x86_64.deb
-sudo apt install /tmp/vagrant.deb
-rm /tmp/vagrant.deb
+curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
+sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+sudo apt-get update && sudo apt install -y vagrant
+
+# Beware if the EFI secure boot is enabled, the kernel modules have to be signed before to be used
+# https://superuser.com/questions/1438279/how-to-sign-a-kernel-module-ubuntu-18-04
